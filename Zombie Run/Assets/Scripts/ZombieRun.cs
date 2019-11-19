3af53e6f737cd54a96f77e3142d07d3fcd600627
KingJerry1 <malyjurek.b@gmial.com>
2019-11-19 02:39:42 +0100

    UI construction and text changing scropt

---
+++ Zombie Run/Assets/Scripts/ZombieRun.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ZombieRun : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text = ("The game starting text");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
